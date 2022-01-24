b6b31df83fdb42d0f96b86fdcb82590374d31ecf
Jakszl <56104691+Jakszl@users.noreply.github.com>
2022-01-24 12:48:50 +0100

    Rename main.cpp to Gra w kości/main.cpp

--- Gra w kości/main.cpp
+++ Gra w kości/main.cpp
@@ -29,7 +29,7 @@ int main()
 	vector<Scoreboard> players;
 	Dice d1(1),d2(2),d3(3),d4(4),d5(5);
 
-	// Ustawianie iloœci graczy oraz ich nazw
+	// Ustawianie iloÅ“ci graczy oraz ich nazw
 
 	cout << "\t Witaj w grze w kosci!";
 	cout << "\n\n Ilu bedzie graczy? ";
@@ -41,22 +41,22 @@ int main()
     int inumPlayer = numPlayer - '0';
     cin.ignore();
 	for (int i = 0; i < inumPlayer; i++) {
-		cout << "\nWpisz nazwe gracza " << i + 1 << ": ";                               //pêtla do wpisywania nazw graczy
+		cout << "\nWpisz nazwe gracza " << i + 1 << ": ";                               //pÃªtla do wpisywania nazw graczy
         getline(cin, playerName);
 		Scoreboard player(playerName);
 		players.push_back(player);
 	}
-	while(round<ROUNDS){                //g³ówna pêtla programu licz¹ca rundy do 13
+	while(round<ROUNDS){                //gÂ³Ã³wna pÃªtla programu liczÂ¹ca rundy do 13
         round++;
         srand (time(NULL));
         cout<<"\n ******************** Runda numer "<<round<<" ***********************\n\n";
 
         for(int j =0;j<inumPlayer;j++){
             cout<<"\n\n ******************** "<<players[j].name<<",czy jestes gotowy/a? ***********************\n"<<endl;
-            cout<<"\t Wybierz: \n 1) Zagraj \n 2) Wyjdz z gry \n 3) Wypisz tablice wynikow i zagraj\n";                     //Menu wyborów dla gracza na pocz¹tku kolejki
+            cout<<"\t Wybierz: \n 1) Zagraj \n 2) Wyjdz z gry \n 3) Wypisz tablice wynikow i zagraj\n";                     //Menu wyborÃ³w dla gracza na poczÂ¹tku kolejki
             char a = ' ';
             cin>>a;
-            while(a!='1' && a!='2' && a!='3'){                              //Wybór opcji
+            while(a!='1' && a!='2' && a!='3'){                              //WybÃ³r opcji
                 wrongOption();
                 cin>>a;
             }
@@ -71,7 +71,7 @@ int main()
 
             //TURY
 
-            for(int k=0;k<TURN;k++){                        //pêtla obs³uguj¹ca ruch w jednej rundzie dla gracza, który sk³ada siê z 3 tur/rzutów kostk¹
+            for(int k=0;k<TURN;k++){                        //pÃªtla obsÂ³ugujÂ¹ca ruch w jednej rundzie dla gracza, ktÃ³ry skÂ³ada siÃª z 3 tur/rzutÃ³w kostkÂ¹
             cout<<"\n\tTwoj "<<k+1<<" rzut\n";
             d1.rollOne();
             d2.rollOne();
@@ -80,7 +80,7 @@ int main()
             d5.rollOne();
             showHand(d1,d2,d3,d4,d5);
             char g = ' ';
-            if(k==2){                                       //Warunek, ktory przenosi nas automatycznie do wpisywania wyników w momencie gdy rzuciliœmy ju¿ 3 raz koœci¹
+            if(k==2){                                       //Warunek, ktory przenosi nas automatycznie do wpisywania wynikÃ³w w momencie gdy rzuciliÅ“my juÂ¿ 3 raz koÅ“ciÂ¹
                 g ='1';
                 d1.SetChange(false);
                 d2.SetChange(false);
@@ -90,7 +90,7 @@ int main()
             }
 
             char s = rollOption(g);
-            while(s!='1' && s!='2'){                        //Menu wyborów dla gracza po pierwszym i drugim rzucie w turze od linijki 93 do 266
+            while(s!='1' && s!='2'){                        //Menu wyborÃ³w dla gracza po pierwszym i drugim rzucie w turze od linijki 93 do 266
                 wrongOption();
                 s=rollOption(g);
             }
@@ -121,10 +121,10 @@ int main()
                     }
                     diceToScore = stoi(sel);
                 }
-                if(diceToScore<7){                                        //Algorytm do liczenia jedynek, dwójek, trójek, czwórek, pi¹tek i szóstek
+                if(diceToScore<7){                                        //Algorytm do liczenia jedynek, dwÃ³jek, trÃ³jek, czwÃ³rek, piÂ¹tek i szÃ³stek
                     score1 = calcUpperScore(diceToScore,d1,d2,d3,d4,d5);
                 }
-                else if(diceToScore == 7){                                //Alorytm do liczenia 3 takich samych koœci
+                else if(diceToScore == 7){                                //Alorytm do liczenia 3 takich samych koÅ“ci
                     int arr[5];
                     writeToArray(arr,d1,d2,d3,d4,d5);
                     map<int, int>m;
@@ -137,7 +137,7 @@ int main()
                             score1 = calcSumDices(d1,d2,d3,d4,d5);
                     }
                 }
-                else if(diceToScore == 8){                                //Algorytm do liczenia 4 takich samych koœci
+                else if(diceToScore == 8){                                //Algorytm do liczenia 4 takich samych koÅ“ci
                     int arr[5];
                     writeToArray(arr,d1,d2,d3,d4,d5);
                     map<int, int>m;
@@ -173,15 +173,15 @@ int main()
                         }
                     }
                 }
-                else if(diceToScore == 10){                                 //Algorytm do liczenia ma³ego strita
+                else if(diceToScore == 10){                                 //Algorytm do liczenia maÂ³ego strita
                     vector<int> v;
                     score1=smallStraight(v,d1,d2,d3,d4,d5);
                 }
-                else if(diceToScore == 11){                                 //Algorytm do liczenia du¿ego strita
+                else if(diceToScore == 11){                                 //Algorytm do liczenia duÂ¿ego strita
                     vector<int> v;
                     score1=bigStraight(v,d1,d2,d3,d4,d5);
                 }
-                else if(diceToScore == 12){                                 //Algorytm do liczenia genera³a
+                else if(diceToScore == 12){                                 //Algorytm do liczenia generaÂ³a
                     vector<int> v;
                     v.push_back(d1.Getscore());
                     v.push_back(d2.Getscore());
@@ -194,8 +194,8 @@ int main()
 
                 }
                 if(players[j].setScore(diceToScore,score1)==-1){            //Funkcja do wpisywania wyniku, czyli zmiennej score1 do konkretnego pola
-                        goto label5;                                        //Oraz sprawdzaj¹ca czy mo¿na wpisaæ w to pole jak¹œ wartoœæ
-                }                                                           //Gdy nie mo¿na, funkcja odsy³a nas do ponownego wyboru
+                        goto label5;                                        //Oraz sprawdzajÂ¹ca czy moÂ¿na wpisaÃ¦ w to pole jakÂ¹Å“ wartoÅ“Ã¦
+                }                                                           //Gdy nie moÂ¿na, funkcja odsyÂ³a nas do ponownego wyboru
                 break;
 
             }
@@ -204,8 +204,8 @@ int main()
                 cout<<"\tWybierz kosci ktore chcesz zostawic, jesli chcesz zamienic wszystkie kosci wpisz: 6 \n Nie uzywaj spacji:  ";
                 cin>> opt;
 
-            for(unsigned int i=0;i<opt.size();i++){                         //Funkcja do wyboru koœci, które chcemy zostawiæ
-                while(isdigit(opt[i])==false){                              //Oraz sprawdzaj¹ca czy wpisane przez nas wartoœci s¹ prawid³owe
+            for(unsigned int i=0;i<opt.size();i++){                         //Funkcja do wyboru koÅ“ci, ktÃ³re chcemy zostawiÃ¦
+                while(isdigit(opt[i])==false){                              //Oraz sprawdzajÂ¹ca czy wpisane przez nas wartoÅ“ci sÂ¹ prawidÂ³owe
                     wrongOption();
                     d1.SetChange(false);
                     d2.SetChange(false);
@@ -243,15 +243,15 @@ int main()
             }
             }
             }
-            players[j].printScoreboard();                   //Wypisanie tablicy wyników dla konretnego gracza
+            players[j].printScoreboard();                   //Wypisanie tablicy wynikÃ³w dla konretnego gracza
             }
 
         }
-        for(int j = 0;j<inumPlayer;j++){                    //Funkcja do wypisania sumarycznego wyniku gracza oo zakoñczeniu rozgrywki
+        for(int j = 0;j<inumPlayer;j++){                    //Funkcja do wypisania sumarycznego wyniku gracza oo zakoÃ±czeniu rozgrywki
             players[j].printScoreboard();
             cout<<"\n\n"<<players[j].name<<", twoj wynik to: "<<players[j].getTotalScore()<<endl;
         }
-        vector<pair<string, int> > A;                       //Algorytm do utworzenia tablicy wyników wszystkich graczy, którzy brali udzia³ w rozgrywce
+        vector<pair<string, int> > A;                       //Algorytm do utworzenia tablicy wynikÃ³w wszystkich graczy, ktÃ³rzy brali udziaÂ³ w rozgrywce
         map<string, int>w;
         for(int j = 0;j<inumPlayer;j++){
             w[players[j].name]=players[j].getTotalScore();
